70678c8e4305c3caf02220a8758b41ffb4d706f3
nishant <nishantpriya.scorpion@gmail.com>
2025-05-22 23:33:51 +0530

    buttons

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -23,19 +29,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnitConverterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(),
-                    containerColor = Color.LightGray
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = Color.LightGray
 
 
-                ) { innerPadding ->
-                    Column {
-                        Text("help me help me", modifier = Modifier.padding(innerPadding));
-                        Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                        )
-//                        OUt
-                    }
+                )  {
+                    UnitConverter()
+
 
                 }
             }
@@ -64,7 +64,21 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun UnitConverter (){
     Column {
         Text("unit converter")
-        OutlinedTextField(value = "", onValueChange = {})
+        OutlinedTextField(value = "meow", onValueChange = {})
+        Row {
+            Box {
+                
+            }
+//            val context = LocalContext.current
+//            Button(onClick = {
+//                Toast.makeText(
+//                    context, " text  ", Toast.LENGTH_LONG
+//                ).show()
+//            }) {
+//                Text("my button")
+//            }
+        }
+        Text("Result: ")
     }
 }
 
