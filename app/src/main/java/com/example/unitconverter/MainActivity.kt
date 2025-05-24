9651d4d93ce98de7a8795b27e8f16808d0213ec8
nishant <nishantpriya.scorpion@gmail.com>
2025-05-24 22:42:44 +0530

    complete

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -26,6 +26,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,6 +37,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UnitConverterTheme {
                 Surface(modifier = Modifier.fillMaxSize(),
-                    color = Color.White
+                    color = Color.DarkGray
 
 
                 )  {
@@ -73,40 +78,87 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun UnitConverter (){
+    //these are two ways of creating state variables
+//    val statething = remember { mutableStateOf("meow") }
+//    // to access statehing.value
+//    val secStateThing by remember { mutableStateOf("also meow") }
+    // to access... simply secStateTHing
+
+
+
+    //these are my state variables
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters")}
+    var outputUnit by remember { mutableStateOf("Meters") }
+
+
+    var isExpanded by remember { mutableStateOf(false) }
+
+    var isExpanded2 by remember { mutableStateOf(false) }
+
+    var conversionFac = remember { mutableStateOf(0.01) }
+
+
+
+    fun convertUnit(){
+        val inputvaldouble = inputValue.toDoubleOrNull() ?: 0.0
+        val result = (inputvaldouble * conversionFac.value * 100).roundToInt() / 100
+        outputValue = result.toString()
+    }
+
+
     Column(verticalArrangement = Arrangement.Center , horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("unit converter")
+        Text("unit converter", style = MaterialTheme.typography.headlineLarge)
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "meow", onValueChange = {})
+        OutlinedTextField(value = inputValue, onValueChange = {
+            inputValue = it
+        }, label = { Text("mylabel") })
         Spacer(modifier = Modifier.height(16.dp))
 
         Row {
             Box {
-                Button(onClick = {}) {
-                    Text("Select")
+                Button(onClick = {
+                    isExpanded = !isExpanded
+                }) {
+                    Text( if(inputUnit == "") "Select" else inputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "drop down button")
                 }
-                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                DropdownMenu(expanded = isExpanded, onDismissRequest = {
+                    isExpanded = !isExpanded
+                }) {
                     DropdownMenuItem(onClick = {
-
+                        inputUnit =  "Centimeters"
+                        isExpanded = !isExpanded
                     }, text = { Text("Centimeters") })
-                    DropdownMenuItem(text = { Text("Meters")}, onClick = {})
+                    DropdownMenuItem(text = { Text("Meters")}, onClick = {
+                        inputUnit =  "Meters"
+                        isExpanded = !isExpanded
+                    })
 
                 }
 
             }
             Spacer(modifier = Modifier.width(16.dp))
             Box{
-                Button(onClick = {}) {
-                    Text("Select")
+                Button(onClick = {
+                    isExpanded2 = !isExpanded2
+                }) {
+                    Text(if(outputUnit == "") "Select" else outputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "drop down button")
                 }
-                DropdownMenu(expanded = false, onDismissRequest = {}) {
-                    DropdownMenuItem(onClick = {
+                DropdownMenu(expanded = isExpanded2, onDismissRequest = {
+                    isExpanded2 = !isExpanded2
+                }) {
 
+                    DropdownMenuItem(onClick = {
+                        outputUnit =  "Centimeters"
+                        isExpanded2 = !isExpanded2
                     }, text = { Text("Centimeters") })
 
                     DropdownMenuItem(text = { Text("Meters")}, onClick = {
-
+                        outputUnit =  "Meters"
+                        isExpanded2 = !isExpanded2
                     })
 
                 }
